e78feaa880d3bd91fc0f1bfa41617a11e49d6051
LeoSirius <is.li.xiaoyu@qq.com>
2020-05-27 01:44:48 +0800

    update

---
+++ Chapter3_基本概念/3.4_1type.js
@@ -0,0 +1,22 @@
+// 所有值都会归属下述六种类型之一。
+
+// js五种简单类型：
+// - Undefined：只有一个值，即undefined
+// - Null：只有一个值，即null
+// - Boolean
+// - Number
+// - String
+
+// 一种复杂数据类型
+// - Object
+
+// 其他类型转化为Boolean的情况
+//                 转化为true     转化为false
+//----------------------------------------
+// String          非空字符串      空字符串
+// Number          非0，包括无穷   0和NaN
+// Object          任何对象        null
+// Undefined                     undefined
+
+
+
