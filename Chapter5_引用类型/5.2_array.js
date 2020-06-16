bb69bf0e6dcf82b9905f4d1d687461789f68b0eb
LeoSirius <is.li.xiaoyu@qq.com>
2020-06-16 23:44:25 +0800

    update

--- Chapter5_引用类型/5.2_array.js
+++ Chapter5_引用类型/5.2_array.js
@@ -2,16 +2,16 @@
 // 1. 动态大小
 // 2. 保存的类型可不一致
 
-// 类似Object，两种重建方式
+// 类似Object，两种创建方式：构造函数和字面量
 var arr1 = new Array("red", "blue", "green");
 var arr2 = ["1", "2", "3"];
 
-// 检测数组
+// -=-=-=-=-=-=-=-=-=-= 检测数组 -=-=-=-=-=-=-=-=-=-=
 if (Array.isArray(arr1)) {
   console.log("arr1 is array");
 }
 
-// 转换方法
+// -=-=-=-=-=-=-=-=-=-= 转换方法 -=-=-=-=-=-=-=-=-=-=
 
 console.log(arr1.toString());  // 转化为以逗号分隔的字符串
 // join可以指定分隔符
@@ -19,14 +19,14 @@ console.log(arr1.join("|"));   // red|blue|green
 
 
 
-// 栈、队列
+// -=-=-=-=-=-=-=-=-=-= 栈、队列 -=-=-=-=-=-=-=-=-=-=
 // 把数组当做栈用： push(), pop()
-// 把数组当做队列用：pop(), shift()    shift()去掉第一行
+// 把数组当做队列用：pop(), shift()    shift()去掉第一个元素
 // unshift()在头部插入
 
 
 
-// 重排序方法
+// -=-=-=-=-=-=-=-=-=-= 重排序方法 -=-=-=-=-=-=-=-=-=-=
 
 var arr = [2,3,1,4,0];
 arr.reverse();
@@ -36,7 +36,12 @@ arr.sort();
 console.log(arr);   // 0,1,2,3,4   sort也可以指定一个compare函数作为参数
 
 
-// 操作方法
+// -=-=-=-=-=-=-=-=-=-= 操作方法 -=-=-=-=-=-=-=-=-=-=
+
+// - concat
+// - slice
+// - splice
+
 // concat拼接数组, slice()切片。两个方法都不会改变原数组
 var colors1 = ["red", "green"];
 var colors2 = colors1.concat("blue", [1,2,3])
@@ -64,21 +69,26 @@ console.log(arr);          // [1, 2, "aaa", 5]
 
 
 
-// 位置方法indexOf(), lastIndexOf(), 分别从头和尾找索引
+// -=-=-=-=-=-=-=-=-=-= 位置方法 -=-=-=-=-=-=-=-=-=-=
+
+// indexOf(), lastIndexOf(), 分别从头和尾找索引
 var arr = [1,2,32,1];
 console.log(arr.indexOf(1));      // 0
 console.log(arr.lastIndexOf(1));  // 3
 
 
 
-// 迭代方法。这5个方法的参数都是一个函数
-// every(), 每一项都是true，则返回true
-// some(), 存在一项是true，则返回true
+// -=-=-=-=-=-=-=-=-=-= 迭代方法 -=-=-=-=-=-=-=-=-=-=
+
+// 这5个方法的参数都是一个函数
+// every(), 返回true或false。每一项都是true，则返回true。
+// some(), 返回true或false。存在一项是true，则返回true
 // map(), 对每一项，返回函数调用结果组成的数组
 // filter(), 返回函数返回true的项组成的数组
 // forEach(), 对每一项，运行函数，没有返回值
 
-// 归并方法 reduce(), reduceRight() 两个方向相反
+// -=-=-=-=-=-=-=-=-=-= 归并方法 -=-=-=-=-=-=-=-=-=-=
+// reduce(), reduceRight() 两个方向相反
 // 固定的四个参数：pre, cur, index, array
 var arr = [1,2,3,4,5,100];
 var sum = arr.reduce((pre, cur, index, array) => pre + cur);
